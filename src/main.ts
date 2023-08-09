ab7dc4cbb4319be86c591f0e26c56437bc9197b9
Jugraj Singh <jsbains96@gmail.com>
2023-08-09 11:55:11 +0100

    SCSS and HTML is all uptodate for mobile site - issue with loading files on github - issue resolved. TS querySelectors and eventListeners in place - need to code the fuction.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./styles.scss";
+import { morseCode } from "./morse-code.ts";
+
+// query selectors
+const englishTextField = document.querySelector(
+  ".english-container__text-field"
+) as HTMLInputElement;
+const morseCodeTextField = document.querySelector(
+  ".morse-code-container__text-field"
+) as HTMLInputElement;
+
+// if - to check for errors
+if (!englishTextField || !morseCodeTextField) {
+  throw new Error("Issue with getting the selector");
+}
+
+// function to do the translations
+
+// eventListeners
+englishTextField.addEventListener("input", englishText);
