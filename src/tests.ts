e21a472413b881084e7c8b452a489c6a19dd8f99
Jugraj Singh <jsbains96@gmail.com>
2023-08-09 15:10:56 +0100

    all the files are up to date and the website works as it shoudl.

---
+++ src/tests.ts
@@ -0,0 +1,8 @@
+import { englishTextField, morseCodeTextField } from "./querySelectors";
+
+describe("lettersCaps", () => {
+  it("should capitaize the alphabets being typed");
+  const result = lettersCaps("hello");
+
+  expect(result).toBe("HELLO");
+});
